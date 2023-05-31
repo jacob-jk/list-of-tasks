a3c5075b1e94a60efeb4dce427d38c8ff261157f
Kuba Karnath <jacob_jk@wp.pl>
2023-05-31 15:52:59 +0200

    slight css edits and adding new tasks

---
+++ js/script.js
@@ -0,0 +1,51 @@
+{
+    const tasks = [
+        {
+            content: "",
+        }
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="list__Item">
+            ${task.content}
+            </li>
+            `
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+    
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
